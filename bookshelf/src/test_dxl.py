9099190bf7d826481749f99f8b85a9e4b8adfa56
judo0140 <61779427+judo0140@users.noreply.github.com>
2022-04-15 13:41:22 +0900

    Update test_dxl.py

--- bookshelf/src/test_dxl.py
+++ bookshelf/src/test_dxl.py
@@ -28,14 +28,6 @@ init_position=dxl.read(1).call()
 dxl.bringup(2)
 dxl.setting(2, 50, 20)
 init_position2=dxl.read(2).call()
-print(init_position)
-#print(init_position)
-#dxl.goal(1,0)
-#dxl.bringup(2)
-#dxl.setting(2, 50, 20)
-#dxl2.bringup(1)
-#dxl2.setting(1, 50, 20)
-#read2=dxl.read(2)
 
 while 1:
     print("Press key! (w/s/x/esc)")
@@ -43,31 +35,18 @@ while 1:
     if direction == chr(0x1b):
         break
 
+    # 'w' 입력 시
     elif direction == chr(0x77):
         dxl.goal(1, init_position+512)
         dxl.goal(2, init_position2+512)
-#        dxl.goal(2, -500000)
-#        dxl.goal(1, read1.call()-6500)
-#        dxl2.goal(1, -50000)
-#        dxl.goal(1, read1.call()+13795)
-#        dxl.goal(2, read2.call()-13795)
 
+    # 'x' 입력 시
     elif direction == chr(0x78):
         dxl.goal(1, init_position-512)
         dxl.goal(2, init_position2-512)
-#        dxl.goal(2, 500000)
-#        dxl.goal(1, read1.call()+6500)
-#        dxl2.goal(1,50000)
 
+    # 's' 입력 시
     elif direction == chr(0x73):
         dxl.goal(1, init_position)
         dxl.goal(2, init_position2)
-#        dxl.stop(1)
-#        dxl.stop(2)
-#        dxl2.stop(1)
-#        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, 3, ADDR_PRESENT_POSITION)
-#        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, dxl_present_position)
-#        print("PresPos-3 : %01d" % (dxl_present_position) )
-
-
 
